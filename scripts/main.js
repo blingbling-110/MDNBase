062da2fcf715019d2639aed4529ea431a4f7c7be
blingbling-110 <523497359@qq.com>
2020-10-21 22:44:36 +0800

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+function changeImage(s){
+    s.src = s.src.match('images/high-heels-5006440-1_1280.jpg')?
+        "images/high-heels-5006440-2_1280.jpg":"images/high-heels-5006440-1_1280.jpg";
+}
+
+let switchButton = document.querySelector('button');
+let heading = document.querySelector('h1');
+let username;
+
+function setUsername() {
+    username = prompt('请输入您的用户名：');
+    localStorage.setItem('username', username);
+}
+
+//初始化
+if (!localStorage.getItem('username')) {
+    setUsername();
+} else {
+    username = localStorage.getItem('username');
+}
+changeHeading();
+
+function changeHeading() {
+    heading.textContent = username + '，喜欢高跟鞋吗？嘿嘿';
+}
+
+switchButton.onclick = function () {
+    setUsername();
+    changeHeading();
+}
